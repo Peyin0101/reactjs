723c516824a77b9ca11403d88ddbe393e5b2ee4c
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-03-11 15:28:05 +0100

    Refactor App component to implement a counter with increment, decrement, and reset functionality

--- state-en-hooks/src/App.jsx
+++ state-en-hooks/src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [showLogos, setShowLogos] = useState(true);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count <= 0 ? 0 : count - 1);
+  const reset = () => setCount(0);
+
+  const canDecrement = count > 0;
+  const canIncrement = count < 100000000;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>HOW GAY IS IDRIS COUNTER</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button disabled={count <= 0} onClick={decrement}>
+        Decrement
+      </button>
+      <button disabled={count == 0} onClick={reset}>
+        Reset
+      </button>
+    </div>
   );
 }
 
