883824f8a388efa0b962f54df8479bf21cfe617c
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-03-28 10:04:58 +0100

    Initialize Vite project with React setup, add Tailwind CSS, and implement ToggleSwitch component

--- styling-componenten/src/ToggleSwitchStart.jsx
+++ styling-componenten/src/ToggleSwitchStart.jsx
@@ -7,14 +7,26 @@ export default function ToggleSwitch() {
     setIsOn(!isOn);
   };
 
+  const toggleStyle = {
+    backgroundColor: isOn ? "rgb(34, 197, 94)" : "rgb(209, 213, 219)",
+  };
+
+  const circleStyle = {
+    transform: isOn ? "translateX(1.5rem)" : "translateX(0)",
+  };
+
   return (
-    <div
-      onClick={handleToggle}
-      className={`relative w-14 h-8 flex items-center rounded-full cursor-pointer p-1 transition-colors duration-300 ease-in-out`}
-    >
+    <div className="grid h-250 place-content-center">
       <div
-        className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ease-in-out`}
-      ></div>
+        style={toggleStyle}
+        onClick={handleToggle}
+        className={`relative w-14 h-8 flex items-center rounded-full cursor-pointer p-1 transition-colors duration-300 ease-in-out`}
+      >
+        <div
+          style={circleStyle}
+          className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ease-in-out`}
+        ></div>
+      </div>
     </div>
   );
 }
